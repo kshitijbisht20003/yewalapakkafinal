262fd428810d7f8623523dd263de9544a604e7fa
kshitij2003 <kshitijbisht2003@gmail.com>
2024-09-29 02:25:20 +0530

    ajfkls

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+// index.js
+import http from 'http';
+
+// Create a server object
+const server = http.createServer((req, res) => {
+    // Set the response header
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    // Write some text to the response
+    res.end('Welcome to my simple Node.js app!');
+});
+
+// Define the port to listen on
+const port = 3000;
+
+// Start the server
+server.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
 
 app.get("/", function(req, res) {
     res.render("index"); // Use "index" as a string
