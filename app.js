c15bed5bea7794087cee0c2a76d2efcfcade10a6
kshitij2003 <kshitijbisht2003@gmail.com>
2024-09-29 02:49:53 +0530

    ajfkls

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
     // Set the response header
     res.writeHead(200, {'Content-Type': 'text/plain'});
     // Write some text to the response
-   res.render("index");
+    res.render("index");
 });
 
 // Define the port to listen on
